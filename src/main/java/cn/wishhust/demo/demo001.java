7667965beb8ad07f50c8d50ca41a5ff7feb3ce65
liuke <18162327131@163.com>
2019-07-02 07:42:19 +0800

    demo01

---
+++ src/main/java/cn/wishhust/demo/demo001.java
@@ -0,0 +1,28 @@
+package main.java.cn.wishhust.demo;
+
+public class demo001 {
+    private static boolean initFlag = false;
+//    private static volatile boolean initFlag = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("等待数据准备");
+                while (!initFlag) {}
+                System.out.println("数据准备完毕，执行程序逻辑");
+            }
+        }).start();
+
+        Thread.sleep(2000);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("数据准备中。。。");
+                initFlag = true;
+                System.out.println("数据准备完成");
+            }
+        }).start();
+    }
+}
