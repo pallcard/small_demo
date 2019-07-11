1bfd1ef24253cd8e98cff9f0b75fb1f49c0bdcda
CurrLiu <Curry.Liu@harman.com>
2019-07-11 11:10:50 +0800

    Demo122

---
+++ src/main/java/cn/wishhust/leetcode/Demo122.java
@@ -0,0 +1,22 @@
+package main.java.cn.wishhust.leetcode;
+
+
+/**
+ *
+ * 若 a<b<c<d, d-a = (d-c) + (c-b) + (b-a)
+ *
+ */
+
+public class Demo122 {
+
+    public int maxProfit(int[] prices) {
+
+        int result = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                result += (prices[i] - prices[i-1]);
+            }
+        }
+        return result;
+    }
+}
