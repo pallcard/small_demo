49bb3d0168f23444ea786f85ed52713af278c8a5
liuke <18162327131@163.com>
2019-07-17 08:43:03 +0800

    提交

---
+++ src/main/java/cn/wishhust/leetcode/Demo017.java
@@ -0,0 +1,40 @@
+package main.java.cn.wishhust.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo017 {
+
+    public static String[] map = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv","wxyz"};
+
+    public static List<String> list = new ArrayList<>();
+
+    public static List<String> letterCombinations(String digits) {
+        StringBuilder sb = new StringBuilder();
+        if (digits.equals("")  || digits == null) {
+            return list;
+        }
+        letterCombinations(digits, sb, 0);
+        return list;
+    }
+
+    public static void letterCombinations(String digits, StringBuilder sb, int k) {
+        if (k == digits.length()) {
+            list.add(sb.toString());
+            return;
+        }
+        for (int i = 0; i < map[digits.charAt(k)-'0'].length(); i++) {
+            sb.append(map[digits.charAt(k)-'0'].charAt(i));
+            letterCombinations(digits, sb, k+1);
+            sb.deleteCharAt(k);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("23"));
+
+    }
+
+}
--- src/main/java/cn/wishhust/leetcode/Demo417.java
+++ src/main/java/cn/wishhust/leetcode/Demo417.java
@@ -7,45 +7,92 @@ import java.util.List;
 public class Demo417 {
 
     public static int [][] direct = new int [][]{{0,-1},{-1,0},{0,1},{1,0}};
-    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+
+    public static List<List<Integer>> pacificAtlantic(int[][] matrix) {
         List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < matrix[0].length; i++) {
-            list.add(Arrays.asList(0, i));
-            list.add(Arrays.asList(i, 0));
+
+        if (matrix.length == 0) {
+            return list;
         }
 
         boolean [][] left = new boolean[matrix.length][matrix[0].length];
+
         for (int i = 0; i < matrix[0].length; i++) {
             left[0][i] = true;
-            pacific(matrix,0, i);
-            left[i][0] = true;
-            pacific(matrix, i, 0);
+            pacificAtlantic(matrix, left, 0, i);
+
         }
 
+        for (int i = 0; i < matrix.length; i++) {
+            left[i][0] = true;
+            pacificAtlantic(matrix, left, i, 0);
+        }
 
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
-                if (i-1 >= 0 && matrix[i][j] > matrix[i-1][j] && left[i-1][j]) {
-                    left[i][j] = true;
-                } else (j-1 >= 0 && matrix[i][j] > matrix[i][j-1] && left[i][j-1])  {
+        boolean [][] right = new boolean[matrix.length][matrix[0].length];
 
+        for (int i = 0; i < matrix[0].length; i++) {
+            right[matrix.length-1][i] = true;
+            pacificAtlantic(matrix, right, matrix.length-1, i);
+
+
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            right[i][matrix[0].length-1] = true;
+            pacificAtlantic(matrix, right, i, matrix[0].length-1);
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (left[i][j] && right[i][j]) {
+                    list.add(Arrays.asList(i,j));
                 }
-//                matrix[i][j] = matrix[i-1][j] || matrix[i][j-1];
             }
         }
+
+        return list;
     }
 
-    public List<List<Integer>> pacific(int[][] matrix, int x, int y) {
-
+    public static void pacificAtlantic(int[][] matrix, boolean[][] left, int x, int y) {
 
         for (int i = 0; i < 4; i++) {
+            int x1 = x+direct[i][0], y1 = y+direct[i][1];
+            if (x1 >= 0 && x1 < matrix.length
+                    && y1 >= 0 && y1 < matrix[0].length
+                    && matrix[x1][y1] >= matrix[x][y] && !left[x1][y1]) {
+                left[x1][y1] = true;
+                pacificAtlantic(matrix, left, x1, y1);
+            }
 
         }
     }
 
 
-    public List<List<Integer>> atlantic(int[][] matrix, int x, int y) {
+//    public static void atlantic(int[][] matrix, boolean[][] left, int x, int y) {
+//        for (int i = 0; i < 4; i++) {
+//            int x1 = x+direct[i][0], y1 = y+direct[i][1];
+//            if (x1 >= 0 && x1 < matrix.length
+//                    && y1 >= 0 && y1 < matrix[0].length
+//                    && matrix[x1][y1] >= matrix[x][y] && left[x1][y1]) {
+//                list.add(Arrays.asList(x1,y1));
+//                pacific(matrix, left, x1, y1);
+//            }
+//
+//        }
+//    }
 
+    public static void main(String[] args) {
+
+        System.out.println(pacificAtlantic(new int [][] {
+                {1,1,1},
+                {1,1,1},
+                {1,1,1},
+//                {1,2,2,3,5},
+//                {3,2,3,4,4},
+//                {2,4,5,3,1},
+//                {6,7,1,4,5},
+//                {5,1,1,2,4}
+        }));
     }
 
 }
