5990ff0ca98224402a6a86c74799cd45f6319d5d
Liu, Curry <Curry.Liu@harman.com>
2019-07-24 09:32:41 +0800

    update

--- src/main/java/cn/wishhust/leetcode/Demo111.java
+++ src/main/java/cn/wishhust/leetcode/Demo111.java
@@ -2,27 +2,39 @@ package main.java.cn.wishhust.leetcode;
 
 public class Demo111 {
 
+//    public int minDepth(TreeNode root) {
+//        if (null == root) {
+//            return 0;
+//        }
+//
+//
+//
+//        int min = Integer.MIN_VALUE;
+//        min = Math.min(minDepth(root.left), minDepth(root.right))+1;
+//        return min;
+//
+//
+//    }
+//
+//    public int minDepthStartWithRoot(TreeNode root) {
+//        if (null == root.left && null == root.right) {
+//            return 1;
+//        }
+//        if (null == root.left || null == root.right) {
+//
+//        }
+//    }
+
     public int minDepth(TreeNode root) {
         if (null == root) {
             return 0;
         }
-
-
-
-        int min = Integer.MIN_VALUE;
-        min = Math.min(minDepth(root.left), minDepth(root.right))+1;
-        return min;
-
-
-    }
-
-    public int minDepthStartWithRoot(TreeNode root) {
-        if (null == root.left && null == root.right) {
-            return 1;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (left == 0 || right == 0) {
+            return left+right+1;
         }
-        if (null == root.left || null == root.right) {
-
-        }
+        return Math.min(left, right)+1;
     }
 
 }
