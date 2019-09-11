3503fe99a84ee1aabab71537333ada12cde440ae
Curry Liu <Curry.Liu@harman.com>
2019-09-11 16:51:58 +0800

    update

--- src/main/java/cn/wishhust/leetcode/Demo095.java
+++ src/main/java/cn/wishhust/leetcode/Demo095.java
@@ -3,6 +3,33 @@ package cn.wishhust.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 95. 不同的二叉搜索树 II
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出:
+ * [
+ *   [1,null,3,2],
+ *   [3,2,null,1],
+ *   [3,1,null,null,2],
+ *   [2,1,3],
+ *   [1,null,2,null,3]
+ * ]
+ * 解释:
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ *
+ */
+
 public class Demo095 {
     public List<TreeNode> generateTrees(int n) {
 
@@ -17,13 +44,16 @@ public class Demo095 {
     public static List<TreeNode> generateTree(int start, int end) {
 
         List<TreeNode> list = new ArrayList<>();
+
         //  根划分后，子树不存在时
         if (start > end) {
             list.add(null);
             return list;
         }
 
-
+        // 1 2 3
+        // 1--0 1 2--3 ;  1--1 2 3--3 ; 1--2 3 4--3
+        //
         for (int i = start; i <= end; i++) {
             // 以i为分割点，（start，i-1），（i+1，end），i为根
             List<TreeNode> left = generateTree(start,i-1);
