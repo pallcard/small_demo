1d7dafae8572c72094117a0cc2a37d6b589c7f07
liuke <18162327131@163.com>
2019-11-27 00:17:32 +0800

    添加标题2

--- src/main/java/cn/wishhust/leetcode/Demo504.java
+++ src/main/java/cn/wishhust/leetcode/Demo504.java
@@ -1,4 +1,22 @@
 package cn.wishhust.leetcode;
+
+/**
+ * 504. 七进制数
+ *
+ * 给定一个整数，将其转化为7进制，并以字符串形式输出。
+ *
+ * 示例 1:
+ *
+ * 输入: 100
+ * 输出: "202"
+ * 示例 2:
+ *
+ * 输入: -7
+ * 输出: "-10"
+ * 注意: 输入范围是 [-1e7, 1e7] 。
+ *
+ *
+ */
 public class Demo504 {
     public String convertToBase7(int num) {
         if (num == 0) {
